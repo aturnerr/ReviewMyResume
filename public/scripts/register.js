6c784ab7f572330c4e95cfcc77694dd477fa37f7
Amie Xie <a.xie4@student,unimelb.edu.au>
2019-04-28 16:19:14 +1000

    registration page updated

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
 
     $userTypeDropdown = $("#type");
+    $countryDropdown = $("#country");
 
     $userTypeDropdown.change(function(){
-        if ($(this).val() == "reviewer") {
+        if ($(this).val() === "reviewer") {
             $(".reviewer-info").css("display", "block");
             $("#occupation").prop("required", true);
             $("#company").prop("required", true);
@@ -11,6 +12,14 @@ $(document).ready(function(){
             $(".reviewer-info").css("display", "none");
             $("#occupation").prop("required", false);
             $("#company").prop("required", false);
+        }
+    });
+
+    $countryDropdown.change(function(){
+        if ($(this).val() === "country"){
+            $(this).css("color", "gray");
+        } else{
+            $(this).css("color", "black");
         }
     });
 })

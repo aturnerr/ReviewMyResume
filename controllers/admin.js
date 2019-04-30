70435c4aca0805877a3f26564d23c02d0f32aa8a
Shevon Mendis <shevonmendis98@gmail.com>
2019-05-01 02:38:41 +1000

    barebones server side validation

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,8 @@
-var User = require("../models/user");
+const User = require("../models/user");
+
+const re_username = /^[a-z0-9_]+$/i;
+const re_text = /^[a-z]+$/i;
+const re_email = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
 exports.admin_show_dashboard = 
 
@@ -32,20 +36,61 @@ exports.admin_logout =
         // logout user and redirect to home page
         req.logout();
         req.flash("success", "Succesfully Logged Out!");
-        res.redirect("/");
+        res.redirect("/login");
     }
 
 exports.admin_register = 
 
     (req, res) => {
 
+        // validate names
+        if (!re_text.test(req.body.user.fname) || 
+                                            !re_text.test(req.body.user.lname)){
+           req.flash("error", "Names must only contain letters!");
+           res.redirect("/register");
+        }
+
+        // validate email
+        if (!re_email.test(req.body.user.email)){
+            req.flash("error", "Invalid Email");
+            res.redirect("/register");
+        }
+
+        if (req.body.user.type === "reviewer"){
+
+            // validate occupation
+            if (!re_text.test(req.body.user.occupation)){
+                req.flash("error", "Invalid occupation!");
+                res.redirect("/register");
+            }
+
+            // validate company
+            if (!re_username.test(req.body.user.company)){
+                req.flash("error", "Invalid company name!");
+                res.redirect("/register");
+            }
+        }
+
+        // validate username
+        if (!re_username.test(req.body.user.username)){
+            req.flash("error", "Usernames can only have letters, numbers and \
+                                underscores!");
+            res.redirect("/register");
+        }
+
+        // validate password 
+        if (req.body.password.trim().length < 8){
+            req.flash("error", "Password too short!");
+            res.redirect("/register");
+        }
+        
+
         // ensure that username is unique
         User.register(new User(req.body.user), req.body.password,
-                                                                function(err, user){
+                                                            function(err, user){
 
             // if there's an error, let the user try again
             if (err){
-                console.log(err);
                 req.flash("error", "Username already in use!");
                 res.redirect('/register');
             }

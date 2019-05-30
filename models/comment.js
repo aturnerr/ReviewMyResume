82abcf29defe4dcc194149b88d7c519e07c633a7
Shevon Mendis <shevonmendis98@gmail.com>
2019-05-31 05:05:15 +1000

    show resume route redesign

--- models/comment.js
+++ models/comment.js
@@ -14,14 +14,6 @@ var CommentSchema = new mongoose.Schema({
       default: Date.now
   },
   type: String,
-  n_upvotes:{
-                type: Number,
-                default: 0
-            },
-  n_downvotes:{
-                type: Number,
-                default: 0
-              }
 });
 
 // return model as object

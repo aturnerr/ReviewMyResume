7beee71976ae5bbbbd11b60a4918a564223a9ef1
Shevon Mendis <shevonmendis98@gmail.com>
2019-05-31 19:15:46 +1000

    UI bug fixes on show resume route

--- public/scripts/show-resume.js
+++ public/scripts/show-resume.js
@@ -16,4 +16,17 @@ $(".r-star.unlocked").hover(function(){
 
     $("#rating").val(n+1);
 
+    var locked_stars = $(".r-star.locked");
+    locked_stars.each(function(){
+        var id_ = $(this).attr("id");
+        var n_ = parseInt(id_.charAt(id_.length-1));
+        if (n_ <= n) {
+            $(this).removeClass("far");
+            $(this).addClass("fas");
+        } else{
+            $(this).removeClass("fas");
+            $(this).addClass("far");
+        }
+    })
+
 });
